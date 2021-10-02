cfc94e66ad4728cf67f9eef5adb5f865cabb61fb
Alessandro Segala (ItalyPaleAle) <43508+ItalyPaleAle@users.noreply.github.com>
2021-10-02 12:07:42 -0700

    Added all control characters Fixes #9

---
+++ src/bad-characters.ts
@@ -0,0 +1,90 @@
+export default [
+    // https://github.com/possan/sublime_unicode_nbsp/blob/master/sublime_unicode_nbsp.py
+    '\x82', // High code comma
+    '\x84', // High code double comma
+    '\x85', // Triple dot
+    '\x88', // High carat
+    '\x91', // Forward single quote
+    '\x92', // Reverse single quote
+    '\x93', // Forward double quote
+    '\x94', // Reverse double quote
+    '\x95', // <control> Message Waiting
+    '\x96', // High hyphen
+    '\x97', // Double hyphen
+    '\x99', // <control>
+    '\xA0', // No-break space
+    '\xA6', // Split vertical bar
+    '\xAB', // Double less than
+    '\xBB', // Double greater than
+    '\xBC', // one quarter
+    '\xBD', // one half
+    '\xBE', // three quarters
+    '\xBF', // c-single quote
+    '\xA8', // modifier - under curve
+    '\xB1', // modifier - under line
+
+    // https://www.cs.tut.fi/~jkorpela/chars/spaces.html
+    '\u1680', // ogham space mark
+    '\u180E', // mongolian vowel separator
+    '\u2000', // en quad
+    '\u2001', // em quad
+    '\u2002', // en space
+    '\u2003', // em space
+    '\u2004', // three-per-em space
+    '\u2005', // four-per-em space
+    '\u2006', // six-per-em space
+    '\u2007', // figure space
+    '\u2008', // punctuation space
+    '\u2009', // thin space
+    '\u200A', // hair space
+    '\u200B', // zero width space
+    '\u200D', // zero width joiner
+    '\u2013', // en dash
+    '\u2014', // em dash
+    '\u2028', // line separator space
+    '\u202F', // narrow no-break space
+    '\u205F', // medium mathematical space
+    '\u3000', // ideographic space
+    '\uFEFF', // zero width no-break space
+    '\uFFFC', // object replacement character
+    '\u00AD', // soft hyphen
+
+    // control characters
+    `\x00`, // NUL
+    `\x01`, // SOH
+    `\x02`, // STX
+    `\x03`, // ETX
+    `\x04`, // EOT
+    `\x05`, // ENQ
+    `\x06`, // ACK
+    `\x07`, // BEL
+    `\x08`, // BS
+    `\x09`, // HT
+    `\x0B`, // VT
+    `\x0C`, // FF
+    `\x0E`, // SO
+    `\x0F`, // SI
+    `\x10`, // DLE
+    `\x11`, // DC1
+    `\x12`, // DC2
+    `\x13`, // DC3
+    `\x14`, // DC4
+    `\x15`, // NAK
+    `\x16`, // SYN
+    `\x17`, // ETB
+    `\x18`, // CAN
+    `\x19`, // EM
+    `\x1A`, // SUB
+    `\x1B`, // ESC
+    `\x1C`, // FS
+    `\x1D`, // GS
+    `\x1E`, // RS
+    `\x1F`, // US
+    `\x7F`, // DEL
+
+    // others
+    '\u037E', // greek question mark
+    '\u00B8', // cadilla
+    '\u01C0', // latin letter dental click
+    '\u2223', // divides
+];
--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,7 @@
-'use strict';
-
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import chars from './bad-characters';
 
 // this method is called when vs code is activated
 export function activate(context: vscode.ExtensionContext) {
@@ -10,75 +9,6 @@ export function activate(context: vscode.ExtensionContext) {
 
     const badCharDecorationStyle = vscode.workspace.getConfiguration('highlight-bad-chars').badCharDecorationStyle;
     const badCharDecorationType = vscode.window.createTextEditorDecorationType(badCharDecorationStyle);
-
-    const chars = [
-        // https://github.com/possan/sublime_unicode_nbsp/blob/master/sublime_unicode_nbsp.py
-        '\x82', // High code comma
-        '\x84', // High code double comma
-        '\x85', // Tripple dot
-        '\x88', // High carat
-        '\x91', // Forward single quote
-        '\x92', // Reverse single quote
-        '\x93', // Forward double quote
-        '\x94', // Reverse double quote
-        '\x95', // <control> Message Waiting
-        '\x96', // High hyphen
-        '\x97', // Double hyphen
-        '\x99', // <control>
-        '\xA0', // No-break space
-        '\xA6', // Split vertical bar
-        '\xAB', // Double less than
-        '\xBB', // Double greater than
-        '\xBC', // one quarter
-        '\xBD', // one half
-        '\xBE', // three quarters
-        '\xBF', // c-single quote
-        '\xA8', // modifier - under curve
-        '\xB1', // modifier - under line
-
-        // https://www.cs.tut.fi/~jkorpela/chars/spaces.html
-        // '\u00A0', // no-break space
-        '\u1680', // ogham space mark
-        '\u180E', // mongolian vowel separator
-        '\u2000', // en quad
-        '\u2001', // em quad
-        '\u2002', // en space
-        '\u2003', // em space
-        '\u2004', // three-per-em space
-        '\u2005', // four-per-em space
-        '\u2006', // six-per-em space
-        '\u2007', // figure space
-        '\u2008', // punctuation space
-        '\u2009', // thin space
-        '\u200A', // hair space
-        '\u200B', // zero width space
-        '\u200D', // zero width joiner
-        '\u2013', // en dash
-        '\u2014', // em dash
-        '\u2028', // line separator space
-        '\u202F', // narrow no-break space
-        '\u205F', // medium mathematical space
-        '\u3000', // ideographic space
-        '\uFEFF', // zero width no-break space
-        '\uFFFC', // object replacement character
-        '\u00AD', // soft hypen
-
-        // others
-        '\u037E', // greek question mark
-        '\u0000', // <control>
-        '\u0011', // <control>
-        '\u0012', // <control>
-        '\u0013', // <control>
-        '\u0014', // <control>
-        '\u001B', // <control>
-        '\u0080', // <control>
-        '\u0090', // <control>
-        '\u009B', // <control>
-        '\u009F', // <control>
-        '\u00B8', // cadilla
-        '\u01C0', // latin letter dental click
-        '\u2223', // divides
-    ];
 
     const additionalChars = vscode.workspace.getConfiguration('highlight-bad-chars').additionalUnicodeChars;
     const charRegExp = '[' + chars.join('') + additionalChars.join('') + ']';
